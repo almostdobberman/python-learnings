e868b8c2b2d0ada94baa4aa0776e6d7155ca45e2
AlmostDobberman <ashley@Ashleys-MBP.attlocal.net>
2024-11-23 13:28:45 -0600

    Calculator Project

---
+++ Calculator.py
@@ -0,0 +1,27 @@
+
+
+operations = {
+    "+": "Add",
+    "-": "subtract",
+    "*": "Multiply",
+    "/": "divide",
+}
+
+first_number = int(input("Enter the first number"))
+action = "What action would you like? Enter + - / *"
+second_number = int(input("Enter Second Number"))
+
+def add ():
+    first_number + second_number
+
+def subtract():
+    first_number - second_number
+
+def multiply():
+    first_number + second_number
+
+def divide():
+    first_number / second_number
+
+
+if action in operations:
